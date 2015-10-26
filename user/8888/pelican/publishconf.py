fb3713dd5f8825bde06d78bc64dde7d76177e2d1
40323123 <4032313@gm.nfu.edu.tw>
2015-10-26 19:28:44 +0800

    8888 update

--- user/8888/pelican/publishconf.py
+++ user/8888/pelican/publishconf.py
@@ -7,8 +7,8 @@ from __future__ import unicode_literals
 
 import os
 
-AUTHOR = '9999'
-SITENAME = '2015FALL 9999 CADPA 作業'
+AUTHOR = '8888'
+SITENAME = '2015FALL 8888 CADPA 作業'
 SITEURL = ''
 
 PATH = 'content'
