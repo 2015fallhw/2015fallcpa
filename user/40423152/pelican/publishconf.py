a52781f675664485089b4c6b859c46f94c6f984d
4023152 <40423152@gm.nfu.edu.tw>
2015-11-05 23:02:29 +0800

    40423152

--- user/40423152/pelican/publishconf.py
+++ user/40423152/pelican/publishconf.py
@@ -8,7 +8,7 @@ from __future__ import unicode_literals
 import os
 
 AUTHOR = '40423152'
-SITENAME = '2015FALL 40423152 CPA 作業'
+SITENAME =<font color=green>'2015FALL 40423152 CPA 作業'
 SITEURL = ''
 
 PATH = 'content'
