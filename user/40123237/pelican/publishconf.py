ba8be74bff8c37d9c021a6401749f4951fdcec57
40123237 <40123237@gm.nfu.edu.tw>
2015-11-07 02:39:37 +0800

    2015fallcpa add

--- user/40123237/pelican/publishconf.py
+++ user/40123237/pelican/publishconf.py
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('40123237作業主頁', ''../../''))
+         ('Python', 'http://python.org/'),('40123237作業主頁', '../../'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
