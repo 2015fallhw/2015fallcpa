bd76a2edf4a2e990317fb2f2ca3c36aa77a3587d
40423104 <40423104@gm.nfu.edu.tw>
2015-11-06 12:19:44 +0800

    syuan

--- user/40423104/pelican/publishconf.py
+++ user/40423104/pelican/publishconf.py
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('2015作業主頁', '../../'))
+         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('40423104作業主頁', 'http://40423104.github.io/2015'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
