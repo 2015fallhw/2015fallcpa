b88fc012fd7409e9498e3a2027574fcecc5407a5
unknown <40423105@gm.nfu.edu.tw>
2015-11-06 11:32:42 +0800

    40423105

--- user/40423105/pelican/publishconf.py
+++ user/40423105/pelican/publishconf.py
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('40423105作業主頁', 'http://40423105.github.io/work7'))
+         ('Python', 'http://python.org/'),('40423105個人倉儲', 'https://github.com/coursemdetw'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
