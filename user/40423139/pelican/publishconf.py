1fe22c54bd5f6a62d81a3eb193b069d8956e3b44
unknown <40423125@gm.nfu.edu.tw>
2015-11-14 17:46:10 +0800

    40423125

--- user/40423139/pelican/publishconf.py
+++ user/40423139/pelican/publishconf.py
@@ -7,8 +7,8 @@ from __future__ import unicode_literals
 
 import os
 
-AUTHOR = '9999'
-SITENAME = '2015FALL 9999 CADPA 作業'
+AUTHOR = '40423139'
+SITENAME = '2015FALL 40423139 CPA 作業'
 SITEURL = ''
 
 PATH = 'content'
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('2015作業主頁', '../../'))
+         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('40423139作業倉儲','https://github.com/40423139'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
