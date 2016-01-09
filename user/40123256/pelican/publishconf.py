63f70aab580d9cf87ed76b3226dbe1a3be491964
40123256 <40123256@gm.nfu.edu.tw>
2016-01-09 17:15:24 +0800

    40123256

--- user/40123256/pelican/publishconf.py
+++ user/40123256/pelican/publishconf.py
@@ -8,7 +8,7 @@ from __future__ import unicode_literals
 import os
 
 AUTHOR = '40123256'
-SITENAME = '2015FALL 40123256 CPA 作業'
+SITENAME = '2015FALL 40123256 CPA 作業 (期中報告)'
 SITEURL = ''
 
 PATH = 'content'
@@ -25,8 +25,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('40123256作業主頁', '../../'))
+LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'), ('Python', 'http://python.org/'),  ('2015作業主頁', '../../'), ('[github] 2015fallhw/2015fallcpb', 'https://github.com/2015fallhw/2015fallcpb/tree/gh-pages'), ('[github] 40123256', 'https://github.com/40123256'), ('[vimeo] 40123157', 'https://vimeo.com/user25757242'), ('[github.io] 40123256/cpw', 'http://40123256.github.io/cpw/'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
@@ -49,9 +48,15 @@ DISQUS_SITENAME = "2015fall"
 #GOOGLE_ANALYTICS = ""
 
 # 必須絕對目錄或相對於設定檔案所在目錄
-PLUGIN_PATHS = ['./../../../plugin']
+PLUGIN_PATHS = ['./../plugin']
 PLUGINS = ['liquid_tags.notebook']
 # 目錄設定相對於 reveal 下的 content 目錄
 NOTEBOOK_DIR = 'notebook'
-
+'''
+if not os.path.exists('_nb_header.html'):     
+    import warnings 
+    warnings.warn("_nb_header.html not found.") 
+else: 
+    EXTRA_HEADER = open('_nb_header.html', encoding="utf-8").read()
+'''
 
