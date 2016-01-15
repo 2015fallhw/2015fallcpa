02a0eb407331e0da348247225fb23ba458f1c4f6
40423106 <40423106@gm.nfu.edu.tw>
2016-01-15 10:47:55 +0800

    40423106

--- user/40423106/pelican/publishconf.py
+++ user/40423106/pelican/publishconf.py
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('40423106作業倉儲', 'http://40423106.github.io/w7/'))
+         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('G2分組報告', 'http://2015fallhw.github.io/2015fallcpa/g2_w15.html'),('40423106 個人網頁', 'http://40423106.github.io/w7/'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
