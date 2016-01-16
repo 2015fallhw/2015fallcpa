f026f077db7403495634f640ce33d4ca1064bce4
g0910841123 <g0921887186@yahoo.com.tw>
2016-01-16 19:27:11 +0800

    40423114

--- user/40423114/pelican/publishconf.py
+++ user/40423114/pelican/publishconf.py
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('40423114作業主頁', 'https://github.com/40423114'))
+         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('40423114 個人網頁', 'http://40423104.github.io/2015'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
