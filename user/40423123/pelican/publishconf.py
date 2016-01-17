402122f5e276aeaee8caa159c9589cc9826fabe7
your name <yourname@example.com>
2016-01-17 17:05:34 +0800

    32

--- user/40423123/pelican/publishconf.py
+++ user/40423123/pelican/publishconf.py
@@ -17,7 +17,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('40423123作業倉儲', 'http://40423123.github.io/cpaw2/'))
+         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('40423123個人網頁', 'http://40423123.github.io/cpaw2/'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
