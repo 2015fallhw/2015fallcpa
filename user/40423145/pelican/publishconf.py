d3f2b446568d81e9212ecf8eb29ff998e418bd36
h2itw <40423145@gm.nfu.edu.tw>
2016-01-20 03:30:06 +0800

    45

--- user/40423145/pelican/publishconf.py
+++ user/40423145/pelican/publishconf.py
@@ -33,7 +33,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('40423145作業倉儲','https://h2itw.github.io/cpahw/'))
+         ('Python', 'http://python.org/'),('40423145作業倉儲','https://h2itw.github.io/cpahw2/'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
